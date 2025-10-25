27e5864caa098489104729275e24a770396941f0
ALEXIS D. DELA CRUZ <addcx1developer@gmail.com>
2025-10-26 00:09:38 +0800

    Define a GraphQL object type named Query, with a latestPost field

--- main.go
+++ main.go
@@ -1,3 +1,21 @@
 package main
 
+import "github.com/graphql-go/graphql"
+
+var queryType = graphql.NewObject(graphql.ObjectConfig{
+	Name: "Query",
+	Fields: graphql.Fields{
+		"latestPost": &graphql.Field{
+			Type: graphql.String,
+			Resolve: func(p graphql.ResolveParams) (interface{}, error) {
+				return "Hello, World!", nil
+			},
+		},
+	},
+})
+
+var Schema, _ = graphql.NewSchema(graphql.SchemaConfig{
+	Query: queryType,
+})
+
 func main() {}
